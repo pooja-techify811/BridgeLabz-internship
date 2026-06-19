68ab66aa8939adb769682c29d9a84eb945dd1cd3
pooja-techify811 <poojasmadu>
2026-06-19 17:31:15 +0530

    Employee Daily wages

---
+++ src/com/bridgelabs/EmployeeWages/DailyEmployeeWages.java
@@ -0,0 +1,14 @@
+package com.bridgelabs.EmployeeWages;
+
+public class DailyEmployeeWages {
+
+    public static void main(String[] args) {
+
+        int wagePerHour = 20;
+        int fullDayHours = 8;
+
+        int dailyWage = wagePerHour * fullDayHours;
+
+        System.out.println("Daily Employee Wage: " + dailyWage);
+    }
+}
