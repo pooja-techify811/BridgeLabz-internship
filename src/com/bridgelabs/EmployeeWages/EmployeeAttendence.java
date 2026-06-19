33347dff7ec050e0960148482b666fc71b70261a
pooja-techify811 <poojasmadu>
2026-06-19 17:25:35 +0530

    Employee Attendence

---
+++ src/com/bridgelabs/EmployeeWages/EmployeeAttendence.java
@@ -0,0 +1,16 @@
+package com.bridgelabs.EmployeeWages;
+import java.util.Random;
+public class EmployeeAttendence {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int attendance = random.nextInt(2);
+
+        if (attendance == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+    }
+}
